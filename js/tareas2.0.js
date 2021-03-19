94566876106d96ae6e31194ef60f173d29a80d45
Gabo-div <64453625+Gabo-div@users.noreply.github.com>
2021-03-19 15:32:18 -0400

    Update tareas2.0.js

--- js/tareas2.0.js
+++ js/tareas2.0.js
@@ -7,7 +7,7 @@ let arrTareas = [];
 //service worker
 if ('serviceWorker' in navigator) {
     
-    navigator.serviceWorker.register('../pwa.js')
+    navigator.serviceWorker.register('./pwa.js')
       .then(reg => console.log('Registro de SW exitoso', reg))
       .catch(err => console.warn('Error al tratar de registrar el sw', err))
 
